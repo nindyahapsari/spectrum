7a65a07510f2ef25feef0dfa0153de393f7da051
Frederico Rose <fredericoa.rose@gmail.com>
2024-02-27 10:40:43 +0100

    SearchBar basic function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Footer />
+      <SearchBar />
     </div>
   )
 }
