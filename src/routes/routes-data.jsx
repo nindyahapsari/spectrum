4311a772d4a008d11c374669a24f1fd9c74bd48d
Emmanouil Popas <noah.popas@hotmail.com>
2024-02-28 15:30:04 +0100

    ErrorPage & Links & Searchbar & CSS

--- src/routes/routes-data.jsx
+++ src/routes/routes-data.jsx
@@ -1,7 +1,9 @@
 import LandingPage from '../Pages/LandingPage'
 import HomePage from '../Pages/HomePage'
+import ErrorPage from '../Pages/ErrorPage'
 
 export const routes = [
   { path: '/landingpage', Element: LandingPage },
   { path: '/', Element: HomePage },
+  { path: '*', Element: ErrorPage },
 ]
