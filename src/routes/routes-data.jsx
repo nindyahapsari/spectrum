4f6d3ce3cd02f9b8781f734d22640ea6f2ae122d
Emmanouil Popas <noah.popas@hotmail.com>
2024-03-04 12:34:44 +0100

    Signup Page & Login & CSS

--- src/routes/routes-data.jsx
+++ src/routes/routes-data.jsx
@@ -2,10 +2,14 @@ import LandingPage from '../Pages/LandingPage'
 import HomePage from '../Pages/HomePage'
 import ErrorPage from '../Pages/ErrorPage'
 import AboutPage from '../Pages/AboutPage'
+import LoginPage from '../Pages/LoginPage'
+import SignupPage from '../Pages/SignupPage'
 
 export const routes = [
   { path: '/landingpage', Element: LandingPage },
   { path: '/', Element: HomePage },
   { path: '/about', Element: AboutPage },
+  { path: '/signup', Element: SignupPage },
+  { path: '/login', Element: LoginPage },
   { path: '*', Element: ErrorPage },
 ]
