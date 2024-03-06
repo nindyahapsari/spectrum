3beebef18615afd5b194b0c80cfe2f33ddf0c660
Frederico Rose <fredericoa.rose@gmail.com>
2024-03-06 15:54:45 +0100

    SearchBar filtering progress

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,21 +1,129 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './SearchBar.css'
-import PropTypes from 'prop-types'
 import Flights from '../Pages/FlightsPage'
 
 const URL = 'http://localhost:5005'
 
+// const SearchBar = () => {
+//   const [input, setInput] = useState('')
+//   const [selectedCategory, setSelectedCategory] = useState('All')
+//   const [resultsFromFlightsAll, setresultsFromFlightsAll] = useState([])
+
+//   const fetchFlight = () => {
+//     axios
+//       .get(`${URL}/api/flights/all`)
+//       .then((response) => {
+//         // setresultsFromFlightsAll(response.data)
+//         console.log('Response =>', response)
+//       })
+//       .catch((error) => {
+//         console.log('Error =>', error)
+//       })
+//   }
+
+//   useEffect(() => {
+//     const runTheFunction = setTimeout(() => {
+//       fetchFlight()
+//     }, 500)
+
+//     return () => {
+//       clearTimeout(runTheFunction)
+//     }
+//   }, [])
+
+//   const handleSearch = () => {
+//     if (resultsFromFlightsAll.length) {
+//       const filteredResults = filterData(resultsFromFlightsAll)
+
+//       setresultsFromFlightsAll(filteredResults)
+//     }
+//   }
+//   const filterData = (apiData) => {
+//     try {
+//       return apiData.filter(
+//         (item) =>
+//           (item.airline?.toLowerCase() ?? '').includes(input.toLowerCase()) &&
+//           (selectedCategory === 'All' || item.destination === selectedCategory),
+//       )
+//     } catch (error) {
+//       console.error('Error filtering data:', error)
+//       return []
+//     }
+//   }
+//   console.log('Search input =>', input)
+//   const handleCategoryChange = (e) => {
+//     setSelectedCategory(e.target.value)
+//   }
+
+//   return (
+//     <div className="search-container">
+//       <input
+//         type="text"
+//         placeholder="Where to?"
+//         value={input}
+//         onChange={(e) => setInput(e.target.value)}
+//         className="search-input"
+//       />
+//       <select
+//         value={selectedCategory}
+//         onChange={handleCategoryChange}
+//         className="category-dropdown"
+//       >
+//         <option value="All">All Categories</option>
+//       </select>
+//       <button onClick={handleSearch} className="search-button">
+//         Search
+//       </button>
+//       {resultsFromFlightsAll.length > 0 ? (
+//         <div>
+//           {resultsFromFlightsAll.map((flights, index) => (
+//             <Flights key={`${flights.destination}-${index}`} {...flights} />
+//           ))}
+//         </div>
+//       ) : (
+//         <div className="no-results-message">No results found.</div>
+//       )}
+//     </div>
+//   )
+// }
+
+// export default SearchBar
+
 const SearchBar = () => {
   const [input, setInput] = useState('')
   const [selectedCategory, setSelectedCategory] = useState('All')
-  const [results, setResults] = useState([])
+  const [resultsFromFlightsAll, setResultsFromFlightsAll] = useState([])
+  const [filteredResults, setFilteredResults] = useState([])
+
+  useEffect(() => {
+    fetchFlight()
+  }, [])
+
+  const fetchFlight = () => {
+    axios
+      .get(`${URL}/api/flights/all`)
+      .then((response) => {
+        console.log('Response =>', response)
+        setResultsFromFlightsAll(response.data)
+      })
+      .catch((error) => {
+        console.log('Error =>', error)
+      })
+  }
+
+  const handleSearch = () => {
+    if (resultsFromFlightsAll.length > 0) {
+      const filteredResults = filterData(resultsFromFlightsAll)
+      setFilteredResults(filteredResults)
+    }
+  }
 
   const filterData = (apiData) => {
     try {
       return apiData.filter(
         (item) =>
-          item.airline.toLowerCase().includes(input.toLowerCase()) &&
+          (item.airline?.toLowerCase() ?? '').includes(input.toLowerCase()) &&
           (selectedCategory === 'All' || item.destination === selectedCategory),
       )
     } catch (error) {
@@ -24,22 +132,13 @@ const SearchBar = () => {
     }
   }
 
-  const handleSearch = async () => {
-    try {
-      const response = await axios.get(`${URL}/api/flights/all`)
-      const apiData = response.data
-      const filteredResults = filterData(apiData)
-      setResults(filteredResults)
-    } catch (error) {
-      console.error('Error fetching data:', error)
-      setResults([])
-    }
-  }
+  console.log('Search input =>', input)
 
   const handleCategoryChange = (e) => {
     setSelectedCategory(e.target.value)
   }
 
+  console.log('Filtered results =>', filteredResults)
   return (
     <div className="search-container">
       <input
@@ -49,7 +148,6 @@ const SearchBar = () => {
         onChange={(e) => setInput(e.target.value)}
         className="search-input"
       />
-
       <select
         value={selectedCategory}
         onChange={handleCategoryChange}
@@ -57,28 +155,20 @@ const SearchBar = () => {
       >
         <option value="All">All Categories</option>
       </select>
-
       <button onClick={handleSearch} className="search-button">
         Search
       </button>
-
-      {results.length > 0 ? (
-        <Flights flights={results} />
+      {filteredResults.length > 0 ? (
+        <div>
+          {filteredResults.map((flights, index) => (
+            <Flights key={`${flights.destination}-${index}`} {...flights} />
+          ))}
+        </div>
       ) : (
         <div className="no-results-message">No results found.</div>
       )}
     </div>
   )
-}
-
-SearchBar.propTypes = {
-  data: PropTypes.arrayOf(
-    PropTypes.shape({
-      airline: PropTypes.string.isRequired,
-      destination: PropTypes.string.isRequired,
-      departureTime: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
 }
 
 export default SearchBar
--- src/context/auth.context.jsx
+++ src/context/auth.context.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect, createContext } from 'react'
 import axios from 'axios'
 import PropTypes from 'prop-types'
-
 const URL = 'http://localhost:5005'
 
 const AuthContext = createContext()
 
-function AuthProviderWrapper(props) {
+function AuthProviderWrapper({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [isLoading, setIsLoading] = useState(true)
   const [user, setUser] = useState(null)
@@ -66,7 +65,7 @@ function AuthProviderWrapper(props) {
         logOutUser,
       }}
     >
-      {props.children}
+      {children}
     </AuthContext.Provider>
   )
 }
