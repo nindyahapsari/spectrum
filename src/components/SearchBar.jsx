7ffa565676a0bb502c69b05129e2b85286c81743
Emmanouil Popas <noah.popas@hotmail.com>
2024-03-07 12:46:23 +0100

    styling mostly done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,76 +1,75 @@
-// SearchBar.jsx
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './SearchBar.css';
-import Flights from '../Pages/FlightsPage';
-import DatePicker from 'react-date-picker';
-import 'react-calendar/dist/Calendar.css';
-import 'react-date-picker/dist/DatePicker.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './SearchBar.css'
+import Flights from '../Pages/FlightsPage'
+import DatePicker from 'react-date-picker'
+import 'react-calendar/dist/Calendar.css'
+import 'react-date-picker/dist/DatePicker.css'
 
-const URL = 'http://localhost:5005';
+const URL = 'http://localhost:5005'
 
 const SearchBar = () => {
-  const [fromWhereInput, setFromWhereInput] = useState('');
-  const [toWhereInput, setToWhereInput] = useState('');
-  const [selectedCategory, setSelectedCategory] = useState('All');
-  const [resultsFromFlightsAll, setResultsFromFlightsAll] = useState([]);
-  const [filteredResults, setFilteredResults] = useState([]);
-  const [date, setDate] = useState(new Date());
+  const [fromWhereInput, setFromWhereInput] = useState('')
+  const [toWhereInput, setToWhereInput] = useState('')
+  const [selectedCategory, setSelectedCategory] = useState('All')
+  const [resultsFromFlightsAll, setResultsFromFlightsAll] = useState([])
+  const [filteredResults, setFilteredResults] = useState([])
+  const [date, setDate] = useState(new Date())
 
   useEffect(() => {
-    fetchFlight();
-  }, []);
+    fetchFlight()
+  }, [])
 
   const fetchFlight = () => {
     axios
       .get(`${URL}/api/flights/all`)
       .then((response) => {
-        console.log('Response =>', response);
-        setResultsFromFlightsAll(response.data);
+        console.log('Response =>', response)
+        setResultsFromFlightsAll(response.data)
       })
       .catch((error) => {
-        console.log('Error =>', error);
-      });
-  };
+        console.log('Error =>', error)
+      })
+  }
 
   const handleSearch = () => {
     if (resultsFromFlightsAll.length > 0) {
-      const filteredResults = filterData(resultsFromFlightsAll);
-      setFilteredResults(filteredResults);
+      const filteredResults = filterData(resultsFromFlightsAll)
+      setFilteredResults(filteredResults)
     }
-  };
+  }
 
   const filterData = (apiData) => {
     try {
       return apiData.filter(
         (item) =>
           (item.airline?.toLowerCase() ?? '').includes(
-            fromWhereInput.toLowerCase()
+            fromWhereInput.toLowerCase(),
           ) &&
-          (selectedCategory === 'All' || item.destination === toWhereInput)
-      );
+          (selectedCategory === 'All' || item.destination === toWhereInput),
+      )
     } catch (error) {
-      console.error('Error filtering data:', error);
-      return [];
+      console.error('Error filtering data:', error)
+      return []
     }
-  };
+  }
 
-  console.log('From where input =>', fromWhereInput);
-  console.log('Where to input =>', toWhereInput);
+  console.log('From where input =>', fromWhereInput)
+  console.log('Where to input =>', toWhereInput)
 
   const handleFromWhereChange = (e) => {
-    setFromWhereInput(e.target.value);
-  };
+    setFromWhereInput(e.target.value)
+  }
 
   const handleToWhereChange = (e) => {
-    setToWhereInput(e.target.value);
-  };
+    setToWhereInput(e.target.value)
+  }
 
   const handleCategoryChange = (e) => {
-    setSelectedCategory(e.target.value);
-  };
+    setSelectedCategory(e.target.value)
+  }
 
-  console.log('Filtered results =>', filteredResults);
+  console.log('Filtered results =>', filteredResults)
 
   return (
     <div className="search-container">
@@ -90,20 +89,27 @@ const SearchBar = () => {
             onChange={handleToWhereChange}
             className="search-input"
           />
-          <DatePicker
-            className="date"
-            selected={date}
-            onChange={(date) => setDate(date)}
-          />
+          <div className="date-picker-container">
+            <DatePicker
+              className="date"
+              selected={date}
+              onChange={(date) => setDate(date)}
+            />
+            <DatePicker
+              className="date"
+              selected={date}
+              onChange={(date) => setDate(date)}
+            />
+          </div>
         </div>
       </div>
       <div className="additional-options">
         <div className="checkbox-container">
-          <label>
+          <label className="label">
             One Way
             <input type="checkbox" />
           </label>
-          <label>
+          <label className="label">
             Return
             <input type="checkbox" />
           </label>
@@ -124,17 +130,14 @@ const SearchBar = () => {
       {filteredResults.length > 0 ? (
         <div>
           {filteredResults.map((flights, index) => (
-            <Flights
-              key={`${flights.destination}-${index}`}
-              {...flights}
-            />
+            <Flights key={`${flights.destination}-${index}`} {...flights} />
           ))}
         </div>
       ) : (
         <div className="no-results-message"></div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default SearchBar;
+export default SearchBar
