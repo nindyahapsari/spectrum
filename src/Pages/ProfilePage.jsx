95dbbf862fba8b9864b213728d4303b62ea37826
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-03-10 22:34:44 +0100

    more changes

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -3,14 +3,17 @@ import { AuthContext } from '../context/Auth.context'
 import ProfileCard from '../components/ProfileCard'
 import TransactionsTable from '../components/TransactionsTable'
 
+import firstIcon from '../assets/airline-icon-first.png'
+import secondIcon from '../assets/airline-icon-second.png'
+
 import { Typography, Spinner } from '@material-tailwind/react'
 
 const TABLE_HEAD = ['Transaction', 'Amount', 'Date', 'Status', 'Account', '']
 
 const TABLE_ROWS = [
   {
-    img: 'https://docs.material-tailwind.com/img/logos/logo-spotify.svg',
-    name: 'Spotify',
+    img: firstIcon,
+    name: 'Air1234',
     amount: '$2,500',
     date: 'Wed 3:00pm',
     status: 'paid',
@@ -19,8 +22,8 @@ const TABLE_ROWS = [
     expiry: '06/2026',
   },
   {
-    img: 'https://docs.material-tailwind.com/img/logos/logo-amazon.svg',
-    name: 'Amazon',
+    img: secondIcon,
+    name: 'Lufthansa',
     amount: '$5,000',
     date: 'Wed 1:00pm',
     status: 'paid',
