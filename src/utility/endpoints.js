212246b841a4c3d1d083211c4f6329029bfcb333
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-03-18 16:41:29 +0100

    delete comment

--- src/utility/endpoints.js
+++ src/utility/endpoints.js
@@ -4,5 +4,3 @@ export const TICKETS_API = `${BASE_URL}/api/tickets`
 export const PURCHASE_API = `${BASE_URL}/api/tickets/purchase`
 
 export const FLIGHTS_ALL_API = `${BASE_URL}/api/flights/all`
-
-// Add more endpoints as needed
