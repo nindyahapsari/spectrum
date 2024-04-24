b8a07a2aa52275ccfc4046f9305a696cbb036402
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-04-24 17:22:43 +0200

    add temporary style

--- src/components/FirstStep.tsx
+++ src/components/FirstStep.tsx
@@ -1,4 +1,4 @@
-import  "../components/FirstStep.css"
+import '../components/FirstStep.css'
 
 const FirstStep = () => {
   return (
@@ -6,11 +6,11 @@ const FirstStep = () => {
       <div className="box find-box">
         <h1>01</h1>
         <h2 className="h2">FIND</h2>
-        <p>
+        <p className="small-text">
           Effortlessly discover tailored flights that match your preferences and
           travel needs with our intuitive search feature.
         </p>
-        <p>
+        <p className="small-text">
           Your journey begins with a seamless and personalized search
           experience, ensuring you find the ideal options for your travel
           adventure.
@@ -20,11 +20,11 @@ const FirstStep = () => {
       <div className="box compare-box">
         <h1>02</h1>
         <h2 className="h2">COMPARE</h2>
-        <p>
+        <p className="small-text">
           Compare ratings, read passenger reviews, and weigh amenities to make
           informed choices.
         </p>
-        <p>
+        <p className="small-text">
           Our comprehensive feature empowers you to select the airline that best
           aligns with your preferences, promising an unparalleled travel
           experience.
@@ -34,12 +34,12 @@ const FirstStep = () => {
       <div className="box choose-box">
         <h1>03</h1>
         <h2 className="h2">CHOOSE</h2>
-        <p>
+        <p className="small-text">
           Choose the perfect flight from a curated list that meets your
           criteria, prioritizing comfort, in-flight services, or budget
           considerations.
         </p>
-        <p>
+        <p className="small-text">
           Elevate your travel experience by making a decision that resonates
           with your unique preferences.
         </p>
