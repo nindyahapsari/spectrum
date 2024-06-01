24d42b07396f35316ab1e56c7c92a2ba63ae3e53
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-06-01 23:02:14 +0200

    uncomment route links as the next component to refactor

--- src/routes/routes-data.tsx
+++ src/routes/routes-data.tsx
@@ -2,8 +2,8 @@ import LandingPage from '../Pages/LandingPage';
 import HomePage from '../Pages/HomePage';
 import ErrorPage from '../Pages/ErrorPage';
 // import AboutPage from '../Pages/AboutPage';
-// import LoginPage from '../Pages/LoginPage';
-// import SignupPage from '../Pages/SignupPage';
+import LoginPage from '../Pages/LoginPage';
+import SignupPage from '../Pages/SignupPage';
 // import Cart from '../Pages/Cart';
 // import ConfirmationPage from '../Pages/ConfirmationPage';
 // import FlightsPage from '../components/Flights';
@@ -15,8 +15,8 @@ export const routes = [
   { path: '/landingpage', Element: LandingPage },
   { path: '/', Element: HomePage },
   // { path: '/about', Element: AboutPage },
-  // { path: '/signup', Element: SignupPage },
-  // { path: '/login', Element: LoginPage },
+  { path: '/signup', Element: SignupPage },
+  { path: '/login', Element: LoginPage },
   // { path: '/cart', Element: Cart },
   // { path: '/confirmation', Element: ConfirmationPage },
   // { path: '/flights', Element: FlightsPage },
