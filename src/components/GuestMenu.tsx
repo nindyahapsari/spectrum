f8ca10e16deb4551b931eb8f56db086ede8c9246
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-06-01 23:01:38 +0200

    add all smaller navbar components

---
+++ src/components/GuestMenu.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import NavbarLink from './NavbarLinks';
+
+type GuestMenuProps = {
+  isOpen: boolean;
+};
+
+const links = [
+  { to: '/signup', label: 'Signup' },
+  { to: '/login', label: 'Login' },
+];
+
+function GuestMenu({ isOpen }: GuestMenuProps) {
+  return (
+    <div className={`navbar-links ${isOpen ? 'active' : ''}`}>
+      {links.map((link) => (
+        <NavbarLink key={link.to} to={link.to} label={link.label} />
+      ))}
+    </div>
+  );
+}
+
+export default GuestMenu;
