fc2e03731324df364aea545125aecad1f3119bbc
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-06-02 20:57:04 +0200

    change footer style to basic style

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Typography } from '@material-tailwind/react';
 
 import './Footer.css';
 
@@ -28,23 +27,23 @@ function Footer() {
           <div className="grid grid-cols-3 justify-between gap-4">
             {LINKS.map(({ title, items }) => (
               <ul key={title}>
-                <Typography
+                <div
                   variant="small"
                   color="black"
                   className="mb-3 font-medium pl-0"
                 >
                   {title}
-                </Typography>
+                </div>
                 {items.map((link) => (
                   <li key={link}>
-                    <Typography
+                    <div
                       as="a"
                       href="#"
                       color="gray"
                       className="py-1.5 font-normal transition-colors hover:text-blue-gray-900"
                     >
                       {link}
-                    </Typography>
+                    </div>
                   </li>
                 ))}
               </ul>
@@ -52,16 +51,16 @@ function Footer() {
           </div>
         </div>
         <div className="mt-12 flex w-full flex-col items-center justify-center border-t border-black py-4 md:flex-row md:justify-between">
-          <Typography
+          <div
             variant="small"
             className="mb-4 text-center font-normal text-blue-gray-900 md:mb-0"
           >
             &copy; {currentYear}{' '}
             <a href="https://material-tailwind.com/">Spectrum</a>. All Rights
             Reserved.
-          </Typography>
+          </div>
           <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
-            <Typography
+            <div
               as="a"
               href="#"
               className="opacity-80 transition-opacity hover:opacity-100"
@@ -78,8 +77,8 @@ function Footer() {
                   clipRule="evenodd"
                 />
               </svg>
-            </Typography>
-            <Typography
+            </div>
+            <div
               as="a"
               href="#"
               className="opacity-80 transition-opacity hover:opacity-100"
@@ -96,8 +95,8 @@ function Footer() {
                   clipRule="evenodd"
                 />
               </svg>
-            </Typography>
-            <Typography
+            </div>
+            <div
               as="a"
               href="#"
               className="opacity-80 transition-opacity hover:opacity-100"
@@ -110,8 +109,8 @@ function Footer() {
               >
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
               </svg>
-            </Typography>
-            <Typography
+            </div>
+            <div
               as="a"
               href="#"
               className="opacity-80 transition-opacity hover:opacity-100"
@@ -128,8 +127,8 @@ function Footer() {
                   clipRule="evenodd"
                 />
               </svg>
-            </Typography>
-            <Typography
+            </div>
+            <div
               as="a"
               href="#"
               className="opacity-80 transition-opacity hover:opacity-100"
@@ -146,7 +145,7 @@ function Footer() {
                   clipRule="evenodd"
                 />
               </svg>
-            </Typography>
+            </div>
           </div>
         </div>
       </div>
