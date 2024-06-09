33a3a56331fc333554f156828369ad6ec3c4e1df
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-06-09 15:20:23 +0200

    refactor: Remove unused ConfirmationPage component and related files

--- src/routes/routes-data.tsx
+++ src/routes/routes-data.tsx
@@ -3,7 +3,6 @@ import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
 import SignupPage from '../pages/SignupPage';
 import Cart from '../pages/CartPage';
-// import ConfirmationPage from '../Pages/ConfirmationPage';
 import FlightsPage from '../components/flights/Flights';
 // import ResultPage from '../Pages/ResultsPage';
 import ProfilePage from '../pages/ProfilePage';
@@ -16,7 +15,6 @@ export const routes = [
   { path: '/signup', Element: SignupPage },
   { path: '/login', Element: LoginPage },
   { path: '/cart', Element: Cart },
-  // { path: '/confirmation', Element: ConfirmationPage },
   { path: '/flights', Element: FlightsPage },
   // { path: '/resultpage', Element: ResultPage },
   { path: '/profile', Element: ProfilePage },
