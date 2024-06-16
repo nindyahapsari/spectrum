dbddfce46c8b1aaa0a763483d338e3393c28e6cf
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-06-16 19:02:04 +0200

    refactor: Remove unused code and update search functionality in HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,48 +1,10 @@
-import { useState, useEffect, useCallback, useContext } from 'react';
-
-import { DataSourceContext } from '../context/DataSource.context';
-
-import { Flight } from '../types';
-
 import InfoCard from '../components/cards/InfoCard';
 import SearchBar from '../components/common/SearchBar/SearchBar';
-// import Flights from '../components/flights/Flights';
-
-type ApiData<T> = T[];
 
 function HomePage() {
-  const [flights, setFlights] = useState<ApiData<Flight>>([]);
-  // const [filteredResults, setFilteredResults] = useState<Flight[]>([]);
-
-  const initFlightsData = useContext(DataSourceContext);
-
-  useEffect(() => {
-    setFlights(initFlightsData);
-  }, [initFlightsData]);
-
-  const filterData = useCallback(function (
-    apiData: ApiData<Flight>,
-    query: string
-  ) {
-    return apiData.filter((data) =>
-      data.destination?.toLowerCase().includes(query)
-    );
-  }, []);
-
-  useEffect(() => {
-    // const query = destinationInput.trim().toLowerCase();
-    // const results = filterData(flights, query);
-    // setFilteredResults(results);
-  }, [flights, filterData]);
-
   function renderContent() {
-    // if (destinationInput !== '' && filteredResults.length > 0) {
-    //   return <Flights filteredResults={filteredResults} />;
-    // }
-
-    // if (destinationInput.trim() !== '') {
-    //   return <p>No flights found for the specified destination</p>;
-    // }
+    // If there is no search query, show the InfoCard
+    // Otherwise, show the search results
 
     return <InfoCard />;
   }
