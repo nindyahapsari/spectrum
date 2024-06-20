ae47d101f6a713381a9c286dfe0e7025cb6f523b
nindya/winona <nindyahapsariiljas@yahoo.com>
2024-06-20 18:37:33 +0200

    fix: fix comparepage and cards with new data

--- src/pages/ComparisonPage.tsx
+++ src/pages/ComparisonPage.tsx
@@ -9,19 +9,22 @@ function ComparisonPage() {
     CompareTicketsContext
   ) as CompareTicketsContextType;
 
+  console.log('ticketsToCompare', ticketsToCompare);
+
   return (
     <div className="p-4 flex flex-col justify-center">
       <div className="text-center text-2xl font-bold my-20">
-        Compare your tickets
+        Compare your flights
       </div>
       <div className="my-5 flex flex-row flex-wrap justify-center">
         {ticketsToCompare?.map(
-          ({ _id, airline, destination, price }: Flight) => (
+          ({ _id, airline, city, flight_number, price }: Flight) => (
             <CompareTicketCard
               key={_id}
               id={_id ?? 'N.A'}
               airline={airline ?? 'N.A'}
-              destination={destination}
+              city={city ?? 'N.A'}
+              flightNumber={flight_number ?? 'N.A'}
               price={price}
             />
           )
